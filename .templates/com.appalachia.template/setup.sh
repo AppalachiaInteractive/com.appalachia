d4a5cd7c3c59001779716af5ad3df2400f13352f
Chris Schubert <chris.a.schubert25@gmail.com>
2021-04-13 16:39:54 -0400

    Updating structure and ignores

--- .templates/com.appalachia.template/setup.sh
+++ .templates/com.appalachia.template/setup.sh
@@ -1,3 +1,16 @@
 echo 'Setting up repository...'
-python .meta/setup.py
-echo 'Repository setup omplete...'
+cd "${0%/*}"
+
+base=basename "$PWD"
+
+echo "# $base" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/AppalachiaInteractive/$base.git
+git add .
+git commit -m "initializing organization repository"
+git push -u origin main
+
+echo 'Repository setup complete...'
