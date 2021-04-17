ada5ac67a4e2225fb3eea7491f1844c65a9d7307
Chris Schubert <chris.a.schubert25@gmail.com>
2021-04-17 09:24:15 -0400

    Fixing shell execution setup for npm, explicitly using bash to allow process substitution.

--- ai/cmd/package/preversion.sh
+++ ai/cmd/package/preversion.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+set +o posix
+
 version=$npm_package_version
 
 git fetch -p
 git pull --tags
 
 "${0%/*}"/../../ai.sh docs changelog
-"${0%/*}"/../../ai.sh docs releaselog
 
 git add .
--- ai/cmd/package/version.sh
+++ ai/cmd/package/version.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+set +o posix
+
+"${0%/*}"/../../ai.sh docs releaselog $npm_package_version
 git add .
