3a567f13ed99f0141421cea45d29f9c72a131208
Chris Schubert <chris.a.schubert25@gmail.com>
2021-06-20 13:36:14 -0400

    Updates for figlet

--- appa/configs/paths.sh
+++ appa/configs/paths.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+export PATH="$APPA_HOME/node_modules:$PATH"
 export PATH="$PATH:/c/Program Files/Mozilla Firefox/"
 export PATH="$PATH:/c/Program Files/Beyond Compare 4/"
 export PATH="$PATH:/c/Program Files/7-Zip/"
