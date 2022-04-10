ca9716a58ba1ca5984e6d2373fb2790493cc3ccc
Janice Crowe <janice@appalachiainteractive.com>
2022-04-10 07:48:54 -0400

    Commenting GCP credential warning

--- appa/configs/googlecloud.sh
+++ appa/configs/googlecloud.sh
@@ -8,6 +8,6 @@ APPA_CONFIG_GOOGLECLOUD='LOADED'
 
 GOOGLE_APPLICATION_CREDENTIALS="${APPA_PRIVATE}/keepers-of-creation-829983f094f1.json"
 
-if [ ! -f "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then
-    warn "File \"${GOOGLE_APPLICATION_CREDENTIALS}\" does not exist, and Google Cloud Service accounts will not work."
-fi
+# if [ ! -f "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then
+#     warn "File \"${GOOGLE_APPLICATION_CREDENTIALS}\" does not exist, and Google Cloud Service accounts will not work."
+# fi
